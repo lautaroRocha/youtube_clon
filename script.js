5eb4185e42a4ac368430276276d662483dbb39ee
Lautaro Rocha <lautarovrocha@gmail.com>
2022-10-22 13:22:55 -0300

    added aside

---
+++ script.js
@@ -0,0 +1,9 @@
+const openClose = document.getElementById("open-close"),
+      aside = document.getElementById("aside");
+
+openClose.addEventListener("click",()=>{
+    aside.classList.toggle("desplegar")
+    
+})
+
+
